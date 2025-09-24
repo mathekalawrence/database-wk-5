55743b8c0d19c304b1fb19f1aef056cb64fe0963
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-24 05:56:21 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -11,5 +11,13 @@ GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
 -- Question 4: Change password for user bob to 'P$55!23'
 ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
 
+--To apply priviledges, the following may have to run
+FLUSH PRIVILEGES;
+--Verification queries:
+-- Verify user exists
+SELECT user, host FROM mysql.user WHERE user = 'bob';
+
+-- Verify privileges
+SHOW GRANTS FOR 'bob'@'localhost';
 
 
