665be4e59bd0ef2aa2a4dad2a79a2db0a8848261
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-24 05:50:55 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,2 +1,7 @@
---Database Indexing and Optimization--
+--Database Indexing and Optimization
+-- Question 1: Drop index named IdxPhone from customers table
+DROP INDEX IdxPhone ON customers;
+-- Question 2: Create user bob with password 'S$cu3r3!' restricted to localhost
+CREATE USER 'muia'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
 
